623c31bca8b62c3079219a51c2bb046f9bb37167
vga01-250026-ai <vga01-wi250026@sankogakuen.jp>
2026-06-12 16:12:05 +0900

    ２回目

--- history.js
+++ history.js
@@ -1,5 +1,6 @@
 // ⚠️ 各ターンの終わりに、画面に表示された自分の結果コードをこの配列の中に追加してください。
 const gameHistory = [
     // ここから下に追記していく
-
-];
+    { turn: 1, player: "playerb", finalPos: "大和", event: "🎉 第1の伝説のお宝をついに発見！！ 🎉", foundTreasure: true },
+    { turn: 2, player: "playerB", finalPos: "箱根湯本", event: "🎉 第2の伝説のお宝をついに発見！！ 🎉", foundTreasure: true },
+]
