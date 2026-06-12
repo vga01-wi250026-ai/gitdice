649c40f48349fb084242bc46bb166a7f3178b391
hiraoka0203t <vga01-wi250033@sankogakuen.jp>
2026-06-12 15:40:32 +0900

    1回目

--- history.js
+++ history.js
@@ -1,5 +1,5 @@
 // ⚠️ 各ターンの終わりに、画面に表示された自分の結果コードをこの配列の中に追加してください。
 const gameHistory = [
     // ここから下に追記していく
-
+    { turn: 1, player: "playerA", finalPos: "町田", event: "無事に移動した！", foundTreasure: false },
 ];
